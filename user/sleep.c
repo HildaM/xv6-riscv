aee71f129e7f1d0f69e919a1c496d2342353dfd8
Quan <Hilda_quan@163.com>
2022-09-03 06:41:25 +0000

    FIX: sleep.c bug

--- user/sleep.c
+++ user/sleep.c
@@ -1,11 +1,27 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+int parse_int(char* s) {
+    const char* p = s;
+    for (; *p; p++) {
+        if (*p < '0' || *p > '9') return -1;
+    }
+
+    return atoi(s);     // atoi 字符串转换为int
+}
+
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
+    if (argc != 2) {
         fprintf(2, "ERROR: Sleep time required\n");
         exit(1);
     }
-    sleep(atoi(argv[1]));   // atoi 字符串转换为int
+
+    int time = parse_int(argv[1]);   
+    if (time < 0) {
+        fprintf(2, "ERROR: time grgument: %s\n", argv[1]);
+        exit(1);
+    }
+
+    sleep(time);   
     exit(0);
 }
